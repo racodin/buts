b0168bbbdad9433b30f16cb161c4d876c772193b
racodin <skkim6.dev@gmail.com>
2021-01-08 18:06:15 +0900

    added pages of sub

--- src/assets/js/buts.js
+++ src/assets/js/buts.js
@@ -1,43 +1,47 @@
-$(function(){
-  var $window = $(window),
-  $header = $('.header');
+$(function () {
+  window.buts = function () {
+    var $window = $(window),
+      $header = $('.header');
 
-  function sticky(){
-    if($window.width() < 900){
-      $header.addClass('fixed');
-    } else {
-      $window.scrollTop() > 35 ? $header.addClass('fixed') : $header.removeClass('fixed');
+    function sticky() {
+      if ($window.width() < 900) {
+        $header.addClass('fixed');
+      } else {
+        $window.scrollTop() > 35 ? $header.addClass('fixed') : $header.removeClass('fixed');
+      }
     }
-  }
 
-  $window.on('scroll', function(){
-    sticky();
-  })
-
-
-  var $menuLi = $('.menu-li'),
-  $subMenu = $('.sub-menu'),
-  $subMenuBg = $('.sub-menu-bg');
-  $.each($menuLi, function(idx, el){
-    $(el).on('mouseenter', function(e){
-      $subMenu.hide().eq(idx).show();
-      $subMenuBg.show();
-    }).on('mouseleave', function(){
-      $subMenu.hide();
-      $subMenuBg.hide();
+    $window.on('scroll', function () {
+      sticky();
     });
-  })
 
-  var $user = $('.header-user'),
-  $right = $('.header-right'),
-  $profile = $('.header-profile');
-  $user.on('click', function(e){
-    e.preventDefault();
-    $profile.show();
-  });
-  $window.on('click', function(e){
-    if(!$right.has(e.target).length){
-      $profile.hide()
-    }
-  })
-})
+    var $menuLi = $('.menu-li'),
+      $subMenu = $('.sub-menu'),
+      $subMenuBg = $('.sub-menu-bg');
+    $.each($menuLi, function (idx, el) {
+      $(el)
+        .on('mouseenter', function (e) {
+          $subMenu.hide().eq(idx).show();
+          $subMenuBg.show();
+        })
+        .on('mouseleave', function () {
+          $subMenu.hide();
+          $subMenuBg.hide();
+        });
+    });
+
+    var $user = $('.header-user'),
+      $right = $('.header-right'),
+      $profile = $('.header-profile');
+    $user.on('click', function (e) {
+      e.preventDefault();
+      $profile.show();
+    });
+    $window.on('click', function (e) {
+      if (!$right.has(e.target).length) {
+        $profile.hide();
+      }
+    });
+  };
+  buts();
+});
