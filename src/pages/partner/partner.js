e89b503a6627eb5d04130d1f3542fe9c08a91a34
racodin <skkim6.dev@gmail.com>
2021-01-22 12:34:46 +0900

    commit

---
+++ src/pages/partner/partner.js
@@ -0,0 +1,31 @@
+$(function () {
+  var $window = $(window),
+    $partnerToggle = $('.partner-toggle'),
+    $partnerShade = $('.partner-shade'),
+    $partnerNav = $('.partner-nav');
+
+  function navToggle() {
+    if ($partnerShade.hasClass('close')) {
+      $partnerShade.add($partnerNav).removeClass('close').addClass('open');
+      $partnerNav.css({ left: '-220px' }).animate({ left: '0' }, 200);
+    } else {
+      $partnerNav.animate({ left: '-220px' }, 200, function () {
+        $partnerNav.css({ left: '' });
+        $partnerShade.add($partnerNav).removeClass('open').addClass('close');
+      });
+    }
+  }
+
+  $partnerToggle.add($partnerShade).on('click', function (e) {
+    e.preventDefault();
+    navToggle();
+  });
+
+  $window.on('resize', function () {
+    if ($window.outerWidth() < 768) {
+      $partnerShade.add($partnerNav).removeClass('open').addClass('close');
+    } else {
+      $partnerShade.add($partnerNav).removeClass('close open');
+    }
+  });
+});
